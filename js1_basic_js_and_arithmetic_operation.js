4a3a4b5dbbaf0dd85c9739acae11c2ed07905fda
mdrashedinislam <121019070+mdrashedinislam@users.noreply.github.com>
2023-01-11 03:20:04 +0600

    Add files via upload

---
+++ js1_basic_js_and_arithmetic_operation.js
@@ -0,0 +1,69 @@
+
+// /**variable */
+// var first_number;
+// let second_number;
+// const third_number = 10;
+// fourth_number = 399;
+
+// first_number =400;
+// second_number =600;
+
+// /**ei nicher gulor madhome inspect console e print hone value gulo */
+// console.log(first_number);
+// console.log(second_number);
+// console.log(third_number);
+// console.log(fourth_number);
+
+
+
+
+let a, b, sum_result, sub_relust, mul_result;
+let div_result, mod_result;
+
+a = 23;
+b = 10;
+sum_result = a + b;
+console.log("sumation : " + sum_result)
+
+
+
+sub_result = a - b;
+console.log("substruction : " + sub_result);
+
+
+mul_result = a * b;
+console.log("multiplication : " + mul_result);
+
+
+div_result = a / b;
+console.log("division : " + div_result);
+
+
+mod_result = a % b;
+console.log("modulus : " + mod_result);
+
+
+
+
+// let c, d, sum_result, sub_result, mul_result; 
+// let div_result, mod_result;
+
+
+// c = 50;
+// d = 25;
+
+// sum_relust = c + d;
+// sub_relust = d - c;
+// mul_relust = c * d;
+// div_relust = d / c;
+// mod_relust = c % d;
+
+
+// console.log("sumation: " + sum_result);
+// console.log("substruction: " + sub_result);
+// console.log("multiplication: " + mul_result);
+// console.log("divition: " + div_result);
+// console.log("modulus: " + mod_result);
+
+
+
